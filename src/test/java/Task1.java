a52671ddb79261017d53863735c6b424428f8163
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-26 19:34:14 +0400

    task1-tsitsikashvili

---
+++ src/test/java/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("isEven(4): " + isEven (4));
+        System.out.println("isEven(7): " + isEven(7));
+    }
+}
