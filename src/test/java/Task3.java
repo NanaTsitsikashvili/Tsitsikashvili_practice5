db815464414a2364954b0c805a7d6a55a78d9eb2
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-26 19:36:58 +0400

    task3-tsitsikashvili

---
+++ src/test/java/Task3.java
@@ -0,0 +1,10 @@
+class CalculateArea {
+    public static void main(String[] args) {
+        calculateArea(5.5, 4.0);
+    }
+
+    static void calculateArea(double სიგრძე, double სიმაღლე) {
+        double area = სიგრძე * სიმაღლე;
+        System.out.println(area);
+    }
+}
